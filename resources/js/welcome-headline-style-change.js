760af4ddba72b3bf7e9c1142046339ba3e6d0a96
Raman Sharma <raman.sharma0096@gmail.com>
2022-04-04 23:25:27 +0200

    Refactoring

--- resources/js/welcome-headline-style-change.js
+++ resources/js/welcome-headline-style-change.js
@@ -1,21 +1,38 @@
 var $welcomeText = $("#welcome-text span");
 var $welcomeContext = $(".welcome-section p:first");
 
-const TIMEOUT_VALUE = 4000;
+const WELCOME_TEXT_TIMEOUT_VALUE = 2000;
 
-$(function () {
-  setTimeout(function () {
-    $welcomeText.animate({
+var executeWelcomeHeadlineStyleChange = () => {
+  var hasRun = localStorage.getItem("hasRun");
+  if (hasRun != "1") {
+    setTimeout(function () {
+      $welcomeText.animate({
+        width: "250px",
+      });
+      $welcomeContext.animate({
+        marginTop: "20px",
+      });
+      $welcomeText.css({
+        background:
+          "-webkit-linear-gradient(45deg, yellow, lightblue, lightgreen)",
+        "-webkit-background-clip": "text",
+        "-webkit-text-fill-color": "transparent",
+      });
+    }, WELCOME_TEXT_TIMEOUT_VALUE);
+    localStorage.setItem("hasRun", "1");
+  } else {
+    $welcomeText.css({
+      background:
+        "-webkit-linear-gradient(45deg, yellow, lightblue, lightgreen)",
+      "-webkit-background-clip": "text",
+      "-webkit-text-fill-color": "transparent",
       width: "250px",
     });
-    $welcomeContext.animate({
-      marginTop: "30px",
+    $welcomeContext.css({
+      marginTop: "20px",
     });
-  }, TIMEOUT_VALUE);
-});
+  }
+};
 
-$(function () {
-  setTimeout(function () {
-    $welcomeText.css({ "background-color": "black", color: "white" });
-  }, TIMEOUT_VALUE);
-});
+executeWelcomeHeadlineStyleChange();
